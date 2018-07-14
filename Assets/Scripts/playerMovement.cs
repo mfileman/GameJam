8846b150866550e566205a8b893fe5a368c2cfd7
Kayton Fletcher <kfletcher1@ufl.edu>
2018-07-14 16:41:34 -0400

    Added movement, started living room

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public float moveSpeed;
+    private Animator animator;
+
+    // Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+	}
+}
